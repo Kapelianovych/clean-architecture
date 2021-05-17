665d38aaef3b591086aea04b08d988f99a30ef42
Kapelianovych Yevhen <kapelianovych.y.v@gmail.com>
2021-05-17 10:33:06 +0300

    Update activity window test.

--- src/domains/entities/__test__/activity_window.entity.spec.ts
+++ src/domains/entities/__test__/activity_window.entity.spec.ts
@@ -9,14 +9,12 @@ describe('activity window entity', () => {
   it('should calculate balance of account', () => {
     const accountId = '57';
 
-    const date = new Date()
-
     const window = createActivityWindow([
-      createActivity('41', '41', accountId, createMoney(8), date, 1),
-      createActivity('41', '41', accountId, createMoney(5), date, 2),
-      createActivity(accountId, accountId, '41', createMoney(3), date, 3),
-      createActivity('41', '41', accountId, createMoney(9), date, 4),
-      createActivity(accountId, accountId, '41', createMoney(1), date, 5),
+      createActivity('41', '41', accountId, createMoney(8)),
+      createActivity('41', '41', accountId, createMoney(5)),
+      createActivity(accountId, accountId, '41', createMoney(3)),
+      createActivity('41', '41', accountId, createMoney(9)),
+      createActivity(accountId, accountId, '41', createMoney(1)),
     ]);
 
     expect(calculateBalance(window, accountId)).toEqual(createMoney(18));
