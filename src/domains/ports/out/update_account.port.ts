203297548e60ade0e51469867af0ba77bfb65b91
Kapelianovych Yevhen <kapelianovych.y.v@gmail.com>
2021-05-19 15:37:55 +0300

    Add test for getAccountBalanceService. Remove use case symbol. Add symbols for out ports. Change .env glob.

--- src/domains/ports/out/update_account.port.ts
+++ src/domains/ports/out/update_account.port.ts
@@ -1,5 +1,7 @@
 import { Account } from '../../entities/account.entity';
 
+export const updateAccountPortSymbol = Symbol('updateAccountPort');
+
 export interface UpdateAccountPort {
   (account: Account): Promise<boolean>;
 }
