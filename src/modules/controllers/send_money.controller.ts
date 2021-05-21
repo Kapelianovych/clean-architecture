3f800767b6b2739f461e202ca425c2ffc74e3693
Kapelianovych Yevhen <kapelianovych.y.v@gmail.com>
2021-05-21 16:54:33 +0300

    Update @prostory/mountain. Split send money controller into small functions and handle situation when there is less parameters then needed.

--- src/modules/controllers/send_money.controller.ts
+++ src/modules/controllers/send_money.controller.ts
@@ -1,6 +1,15 @@
 import { constants } from 'http2';
 
-import { get, accessRequest, responseFor } from '@prostory/mountain';
+import { get, accessRequest, responseFor, Request } from '@prostory/mountain';
+import {
+  none,
+  some,
+  Tuple,
+  tuple,
+  maybe,
+  Option,
+  isNothing,
+} from '@fluss/core';
 
 import { inject } from '../../packages/di/container';
 import { createMoney } from '../../domains/entities/money.entity';
@@ -14,50 +23,71 @@ import {
   AccountResult,
 } from '../../domains/entities/account.entity';
 
+const gatherSearchParameters = (
+  ...parameters: ReadonlyArray<Option<string>>
+): Option<Tuple<string[]>> =>
+  parameters.reduce(
+    (previous: Option<Tuple<string[]>>, current) =>
+      previous.chain((value) =>
+        current.chain((secondValue) =>
+          isNothing(secondValue) ? none : some(value.append(secondValue))
+        )
+      ),
+    some(tuple())
+  );
+
+const success = (request: Request) => () =>
+  responseFor(request)
+    .header(constants.HTTP2_HEADER_STATUS, String(constants.HTTP_STATUS_OK))
+    .body('Succeed. targetAccountId is happy.')
+    .end();
+
+const forbid = (request: Request) => (code: AccountResult) => {
+  responseFor(request)
+    .header(
+      constants.HTTP2_HEADER_STATUS,
+      String(constants.HTTP_STATUS_FORBIDDEN)
+    )
+    .json({
+      code,
+      reason:
+        AccountResult.WITHDRAW_NOT_PERMITTED === code
+          ? 'Source account has not enough money.'
+          : 'Target account cannot receive money.',
+    });
+};
+
+const missedParameters = (request: Request) => () =>
+  responseFor(request)
+    .header(
+      constants.HTTP2_HEADER_STATUS,
+      String(constants.HTTP_STATUS_NOT_ACCEPTABLE)
+    )
+    .body('You did not pass all needed parameters.')
+    .end();
+
 export const sendMoneyController = (
   sendMoneyUseCase: SendMoneyUseCase = sendMoneyService(
     inject(loadAccountPortSymbol),
     inject(updateAccountPortSymbol)
   )
 ) =>
-  get(
-    '/account/send\\?sourceAccountId=(\\d+)&targetAccountId=(\\d+)&amount=(\\d+)',
-    (request) => {
-      const { parameters } = accessRequest(request);
+  get('/account/send', (request) => {
+    const { url } = accessRequest(request);
 
-      const [sourceAccountId, targetAccountId, amount] = parameters;
+    const sourceAccountId = maybe(url.searchParams.get('sourceAccountId'));
+    const targetAccountId = maybe(url.searchParams.get('targetAccountId'));
+    const amount = maybe(url.searchParams.get('amount'));
 
-      sendMoneyUseCase(
-        createSendMoneyCommand(
-          sourceAccountId as AccountId,
-          targetAccountId as AccountId,
-          createMoney(Number(amount))
-        )
-      ).then((result) =>
-        result
-          .map(() =>
-            responseFor(request)
-              .header(
-                constants.HTTP2_HEADER_STATUS,
-                String(constants.HTTP_STATUS_OK)
-              )
-              .body('Succeed. targetAccountId is happy.')
-              .end()
+    gatherSearchParameters(sourceAccountId, targetAccountId, amount)
+      .map(([sourceId, targetId, moneyAmount]) =>
+        sendMoneyUseCase(
+          createSendMoneyCommand(
+            sourceId as AccountId,
+            targetId as AccountId,
+            createMoney(Number(moneyAmount))
           )
-          .handle((code) => {
-            responseFor(request)
-              .header(
-                constants.HTTP2_HEADER_STATUS,
-                String(constants.HTTP_STATUS_FORBIDDEN)
-              )
-              .json({
-                code,
-                reason:
-                  AccountResult.WITHDRAW_NOT_PERMITTED === code
-                    ? 'Source account has not enough money.'
-                    : 'Target account cannot receive money.',
-              });
-          })
-      );
-    }
-  );
+        ).then((result) => result.map(success(request)).handle(forbid(request)))
+      )
+      .fill(missedParameters(request));
+  });
